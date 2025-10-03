f77b0ecafca739f7ea8853da9566f4eef94269ea
Viktor1009 <viktorosterberg10@gmail.com>
2025-10-03 09:48:55 +0200

    tested createing triangles and makeing css for them

---
+++ public/structure/games/gamelogic/shaperunfast.js
@@ -0,0 +1,11 @@
+let jumpHeight = 10;
+
+function MakeSpike() {
+    
+    const righttriangle = document.createElement("div");
+    righttriangle.className = "right";
+
+
+    const lefttriangle = document.createElement("div");
+    lefttriangle.className = "left";
+}
