24a2816ba5d962b35e7d0d9d809651912914f647
Viktor1009 <viktorosterberg10@gmail.com>
2025-10-16 14:13:16 +0200

    The triangles are now moving next step is to make the player jump

---
+++ public/structure/games/shaperunfast/gamelogic/shaperunfast.js
@@ -0,0 +1,35 @@
+function Triangle() {
+    const triangleSpeed = -7;
+
+    function spawnTriangle() {
+        const triangle = document.createElement("div");
+        triangle.className = "triangle";
+
+        const y = window.innerHeight * 0.15;
+        triangle.style.bottom = `${y}px`;
+        let x = window.innerWidth + 20;
+        triangle.style.left = `${x}px`;
+
+        document.body.appendChild(triangle);
+
+        function moveTriangle() {
+            x += triangleSpeed;
+            triangle.style.left = `${x}px`;
+
+            if (x > -50)
+            {
+                requestAnimationFrame(moveTriangle);
+            } 
+            else 
+            {
+                triangle.remove();
+            }
+        }
+
+        requestAnimationFrame(moveTriangle);
+        
+    }
+
+    setInterval(spawnTriangle, 1500);
+}
+
