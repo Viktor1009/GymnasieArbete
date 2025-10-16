617a3d8041619abd281e226949f10c59f12ee0b7
Viktor1009 <viktorosterberg10@gmail.com>
2025-10-16 14:32:46 +0200

    last comit made spawnTriangle unreachable due to putting everything inside a diffrent function. i removed that function and cleaned up the code

--- public/structure/games/shaperunfast/gamelogic/shaperunfast.js
+++ public/structure/games/shaperunfast/gamelogic/shaperunfast.js
@@ -1,35 +1,32 @@
-function Triangle() {
-    const triangleSpeed = -7;
+const triangleSpeed = -7;
+setInterval(spawnTriangle, 1500);
 
-    function spawnTriangle() {
-        const triangle = document.createElement("div");
-        triangle.className = "triangle";
+function spawnTriangle() {
+    const triangle = document.createElement("div");
+    triangle.className = "triangle";
 
-        const y = window.innerHeight * 0.15;
-        triangle.style.bottom = `${y}px`;
-        let x = window.innerWidth + 20;
+    const y = window.innerHeight * 0.15;
+    triangle.style.bottom = `${y}px`;
+    let x = window.innerWidth + 20;
+    triangle.style.left = `${x}px`;
+
+    document.body.appendChild(triangle);
+
+    function moveTriangle() {
+        x += triangleSpeed;
         triangle.style.left = `${x}px`;
 
-        document.body.appendChild(triangle);
-
-        function moveTriangle() {
-            x += triangleSpeed;
-            triangle.style.left = `${x}px`;
-
-            if (x > -50)
-            {
-                requestAnimationFrame(moveTriangle);
-            } 
-            else 
-            {
-                triangle.remove();
-            }
+        if (x > -50)
+        {
+            requestAnimationFrame(moveTriangle);
+        } 
+        else 
+        {
+            triangle.remove();
         }
-
-        requestAnimationFrame(moveTriangle);
-        
     }
 
-    setInterval(spawnTriangle, 1500);
+    requestAnimationFrame(moveTriangle);
+    
 }
 
